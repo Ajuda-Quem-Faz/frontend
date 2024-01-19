682738f1c1a980531bf955f41872bf1af621df90
Wallysson Araujo <Cerberus74@live.com>
2024-01-19 15:12:38 -0300

    About and Contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Contact from './pages/contact/Contact';
+import About from './components/about/About';
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <div className='min-h-[80vh]'>
+          <Routes>
+            <Route path="/contact" element={<Contact/>}/>
+          </Routes>
+        </div>
+        <About />
+      </BrowserRouter>
     </>
   )
 }
