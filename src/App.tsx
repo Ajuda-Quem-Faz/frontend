b8c3fe4091b51a08179690459fc69b7eac28c5f5
Pablo Henrique <pablo.h.s.maia@gmail.com>
2024-01-19 15:42:47 -0300

    NavBar prototipo

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Contact from './pages/contact/Contact';
-import About from './components/about/About';
 import Footer from './components/footer/Footer'
-import NavBar from './components/navbar/Navbar'
+import NavBar from './components/navbar/Navbar';
+import About from './components/about/About';
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-      
-        <div className='min-h-[80vh]'>
-          <Routes>
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </div>
-
-        <About />
-        <NavBar/>
-        <div className="min-h-[80vh]">
-        <Routes>
-
-        </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+        <BrowserRouter>
+        <NavBar />
+          <div className="min-h-[80vh]">
+            <Routes>
+                <Route path="/" element={<Contact />} />
+                  {/* <Route path="/home" element={<Home />} /> */}
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
   )
 }
