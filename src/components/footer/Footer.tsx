1635d5c73a27f795b802e3791a181772eb702f88
Vitor Ferreira <vitornasc673@gmail.com>
2024-02-01 09:20:17 -0300

    Atualização Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,39 +1,31 @@
-import { YoutubeLogo } from "@phosphor-icons/react"
+import { CurrencyCircleDollar, Medal, Storefront, YoutubeLogo } from "@phosphor-icons/react"
 import { FacebookLogo, InstagramLogo, LinkedinLogo } from "@phosphor-icons/react/dist/ssr"
+import { Link } from "react-router-dom"
 
 function Footer() {
     return (
-        <footer className="flex flex-col bg-orange-300">
-            <div className="grid grid-cols-3 px-28 py-2 text-purple-950 font-semibold">
-                <div className="flex flex-col ml-10 ">
-                    <h3 className="text-xl font-semibold py-3">Institucional</h3>
-                    <p className="hover:underline cursor-pointer">Quem somos</p>
-                    <p className="hover:underline cursor-pointer">Carreira</p>
-                    <p className="hover:underline cursor-pointer">Pessoas e Lojas</p>
-                    <p className="hover:underline cursor-pointer">Políticas do Ajuda quem faz</p>
-                </div>
-                <div className="flex flex-col ml-10">
-                    <h3 className="text-xl font-semibold py-3">Dúvidas</h3>
-                    <p className="hover:underline cursor-pointer">Como doar</p>
-                    <p className="hover:underline cursor-pointer">Como comprar</p>
-                    <p className="hover:underline cursor-pointer">Isenção fiscal</p>
-                    <p className="hover:underline cursor-pointer">Trocas e devoluções</p>
-                </div>
-                <div className="flex flex-col ml-10">
-                    <h3 className="text-xl font-semibold py-3">Informação</h3>
-                    <p className="hover:underline cursor-pointer">Meios de Pagamento</p>
-                    <p className="hover:underline cursor-pointer">Como vender</p>
-                    <p className="hover:underline cursor-pointer">Abra sua loja</p>
-                    <p className="hover:underline cursor-pointer">Impacto social</p>
-                </div>
+        <footer className="flex flex-col">
+            <div className="flex justify-around items-center py-2 text-indigo-950 font-semibold">
+                <Link to={'/about'} className="flex flex-col items-center">
+                    <Medal size={36}></Medal>
+                    <p className="">Quem somos</p>
+                </Link>
+                <Link to={''} className="flex flex-col items-center">
+                    <CurrencyCircleDollar size={36}></CurrencyCircleDollar>
+                    <p className="">Como contribuir</p>
+                </Link>
+                <Link to={''} className="flex flex-col items-center">
+                    <Storefront size={36}></Storefront>
+                    <p className="">Como Vender</p>
+                </Link>
             </div>
-            <div className="flex justify-between px-10 py-2">
+            <div className="flex justify-between px-10 py-3 bg-violet-950">
                 <img src="" alt="logo"></img>
-                <div id="icon" className="flex gap-5">
-                    <InstagramLogo size={40} weight="bold" color="purple"></InstagramLogo>
-                    <FacebookLogo size={40} weight="bold" color="purple"></FacebookLogo>
-                    <YoutubeLogo size={40} weight="bold" color="purple"></YoutubeLogo>
-                    <LinkedinLogo size={40} weight="bold" color="purple"></LinkedinLogo>
+                <div id="icon" className="flex gap-8">
+                    <InstagramLogo size={40} color="#F1F1E7"></InstagramLogo>
+                    <FacebookLogo size={40} color="#F1F1E7"></FacebookLogo>
+                    <YoutubeLogo size={40} color="#F1F1E7"></YoutubeLogo>
+                    <LinkedinLogo size={40} color="#F1F1E7"></LinkedinLogo>
                 </div>
             </div>
             <div className="flex justify-between px-8 font-bold text-xs py-1">
