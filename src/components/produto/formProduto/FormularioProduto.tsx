7187fdeca399261c4b1ec20d807210c9aa204ca2
Natan Oliveira Santos <natanoliveirasantosmed@gmail.com>
2024-02-05 11:54:57 -0300

    banner e padronização formularioprodutos

--- src/components/produto/formProduto/FormularioProduto.tsx
+++ src/components/produto/formProduto/FormularioProduto.tsx
@@ -148,102 +148,130 @@ function FormularioProduto() {
 
 
 
-    return (
-        <div className="flex justify-center items-center">
-            <div className="flex justify-center text-dark-black font-semibold py-9">
-                <form
-                    className="flex flex-col justify-center gap-2 w-2/3"
-                    onSubmit={gerarNovoProduto}
-                >
-                    <div className="flex flex-col gap-2">
-                        <label htmlFor="nome">Nome do Produto</label>
-                        <input
-                            type="text"
-                            placeholder="Nome"
-                            name="nome"
-                            className="border-2 border- rounded p-2"
-                            value={produto.nome}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                            required
-                            minLength={5}
-                            maxLength={100}
-                            onInvalid={e => (e.target as HTMLInputElement).setCustomValidity('O nome do produto é obrigatório! Por favor, preencher.')}
-                            onInput={e => (e.target as HTMLInputElement).setCustomValidity('')}
-                        />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <label htmlFor="descricao">Descrição</label>
-                        <input
-                            type="text"
-                            placeholder="Descricao"
-                            name="descricao"
-                            className="border-2 border- rounded p-2"
-                            value={produto.descricao}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                            minLength={0}
-                            maxLength={500}
-                            onInvalid={e => (e.target as HTMLInputElement).setCustomValidity('A descrição não pode ultrapassar dos 500 caracteres.')}
-                            onInput={e => (e.target as HTMLInputElement).setCustomValidity('')}
-                        />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <label htmlFor="preco">Preço</label>
-                        <input
-                            type="text"
-                            placeholder="Preco"
-                            name="preco"
-                            className="border-2 border- rounded p-2"
-                            value={produto.preco}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                            required
-                            onInvalid={e => (e.target as HTMLInputElement).setCustomValidity('O preço é obrigatório e deve ser maior que R$0,50.')}
-                            onInput={e => (e.target as HTMLInputElement).setCustomValidity('')}
-                        />
-                    </div>
-                    <div className="flex flex-col gap-2">
-                        <label htmlFor="foto">Foto</label>
-                        <input
-                            type="foto"
-                            placeholder="Foto"
-                            name="foto"
-                            className="border-2 border- rounded p-2"
-                            value={produto.foto}
-                            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-                        />
-                    </div>
-                    <div className="flex flex-col gap-2">
-          <p>Tema da postagem</p>
-          <select name="categoria" id="categoria" className='border p-2 border-slate-800 rounded' onChange={(e) => buscarCategoriaPorId(e.currentTarget.value)}>
-            <option value="" selected disabled>Selecione um tema</option>
+   // Este é o código que eu juntei e estilizei
+return (
+    <div className="flex justify-center items-center">
+      <div className="flex justify-center text-dark-black font-semibold py-9 md:w-1/4">
+        <form
+          action=""
+          className="flex flex-col justify-center gap-2 w-2/3"
+          onSubmit={gerarNovoProduto}
+        >
+          <div className="flex justify-center text-xl items-center gap-3">
+            <img src="./logo.png" alt="" className="h-12" />
+            <h2 className="font-bold text-center">Ajuda Quem Faz</h2>
+          </div>
+          <hr />
+          <h1 className="py-2 flex justify-center text-2xl">
+            {id === undefined ? "Cadastrar Produto" : "Editar Produto"}
+          </h1>
+          <label className="text-lg">Nome do Produto</label>
+          <input
+            type="text"
+            placeholder="Nome"
+            name="nome"
+            className="border-2 rounded-lg text-base font-normal text-dark-black px-2 py-1" // Eu usei a mesma classe que você usou no primeiro código
+            value={produto.nome}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            required
+            minLength={5}
+            maxLength={100}
+            onInvalid={(e) =>
+              (e.target as HTMLInputElement).setCustomValidity(
+                "O nome do produto é obrigatório! Por favor, preencher."
+              )
+            }
+            onInput={(e) =>
+              (e.target as HTMLInputElement).setCustomValidity("")
+            }
+          />
+          <label className="text-lg">Descrição</label>
+          <input
+            type="text"
+            placeholder="Descricao"
+            name="descricao"
+            className="border-2 rounded-lg text-base font-normal text-dark-black px-2 py-1" // Eu usei a mesma classe que você usou no primeiro código
+            value={produto.descricao}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            minLength={0}
+            maxLength={500}
+            onInvalid={(e) =>
+              (e.target as HTMLInputElement).setCustomValidity(
+                "A descrição não pode ultrapassar dos 500 caracteres."
+              )
+            }
+            onInput={(e) =>
+              (e.target as HTMLInputElement).setCustomValidity("")
+            }
+          />
+          <label className="text-lg">Preço</label>
+          <input
+            type="text"
+            placeholder="Preco"
+            name="preco"
+            className="border-2 rounded-lg text-base font-normal text-dark-black px-2 py-1" // Eu usei a mesma classe que você usou no primeiro código
+            value={produto.preco}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            required
+            onInvalid={(e) =>
+              (e.target as HTMLInputElement).setCustomValidity(
+                "O preço é obrigatório e deve ser maior que R$0,50."
+              )
+            }
+            onInput={(e) =>
+              (e.target as HTMLInputElement).setCustomValidity("")
+            }
+          />
+          <label className="text-lg">Foto</label>
+          <input
+            type="foto"
+            placeholder="Foto"
+            name="foto"
+            className="border-2 rounded-lg text-base font-normal text-dark-black px-2 py-1" // Eu usei a mesma classe que você usou no primeiro código
+            value={produto.foto}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            style={{
+              backgroundImage: `url(${produto.foto || "./default.png"})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          />
+          <label className="text-lg">Tema da postagem</label>
+          <select
+            name="categoria"
+            id="categoria"
+            className="border-2 rounded-lg text-base font-normal text-dark-black px-2 py-1" // Eu usei a mesma classe que você usou no primeiro código
+            onChange={(e) => buscarCategoriaPorId(e.currentTarget.value)}
+          >
+            <option value="" selected disabled>
+              Selecione um tema
+            </option>
             {categorias.map((categoria) => (
               <>
-                <option value={categoria.id} >{categoria.setor}</option>
+                <option value={categoria.id}>{categoria.setor}</option>
               </>
             ))}
           </select>
-        </div>
-                    <button
-                        type='submit'
-                        className='rounded disabled:bg-slate-200 bg-indigo-400 
-                            hover:bg-indigo-800 text-white font-bold w-1/2 
-                            mx-auto py-2 flex justify-center'
-                    >
-                        {isLoading ?
-                            <RotatingLines
-                                strokeColor="white"
-                                strokeWidth="5"
-                                animationDuration="0.75"
-                                width="24"
-                                visible={true}
-                            /> :
-                            <span>{id !== undefined ? 'Atualizar' : 'Cadastrar'}</span>
-                        }
-                    </button>
-                    
-                </form>
-            </div>
-        </div>
-    );
-}
-
-export default FormularioProduto;
+          <button
+            className="border-2 border-solid rounded-lg border-dark-black hover:border-secondary-purple hover:text-secondary-purple
+                   p-1 mt-4 text-lg flex justify-center" // Eu usei a mesma classe que você usou no primeiro código
+            type="submit"
+          >
+            {isLoading ? (
+              <RotatingLines
+                strokeColor="purple"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="28"
+                visible={true}
+              />
+            ) : (
+              <span>{id === undefined ? "Cadastrar" : "Atualizar"}</span>
+            )}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+            }
+  export default FormularioProduto;
