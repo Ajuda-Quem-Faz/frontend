3e08e5b5f3e156cb450a390cae5c93a080b2f31d
Wallysson Araujo <Cerberus74@live.com>
2024-02-05 16:16:09 -0300

    Fix things

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -2,16 +2,17 @@ import { useEffect } from 'react';
 
 function About() {
   useEffect(() => {
-    document.title = 'Ajuda quem Faz - Sobre nós';
+    document.title = 'Ajuda quem Faz';
   }, []);
 
   return (
     <>
       {/* Cards */}
-      <div className="w-full bg-light-grayDark rounded-b-3xl">
-        <div className="p-20 grid grid-cols-1 lg:grid-cols-3 justify-between gap-2">
+      <div className="w-full bg-light-grayDark">
+        <div className="p-10 grid grid-cols-1 lg:grid-cols-3 justify-between gap-2">
+
           {/* Missão */}
-          <div className="bg-secondary-purple grid place-items-center rounded-3xl">
+          <div className="flex flex-col bg-secondary-purple place-items-center justify-center p-2 rounded-3xl">
             <h2 className="mt-[30px] font-bold text-[30px] text-tertiary-blueLight">
               Missão
             </h2>
@@ -22,6 +23,7 @@ function About() {
               alta qualidade, enquanto contribuem diretamente para causas sociais.
             </p>
           </div>
+
           {/* Visão */}
           <div className=" bg-secondary-purple grid place-items-center rounded-3xl">
             <h2 className="mt-[30px] font-bold text-[30px] text-tertiary-blueLight">
