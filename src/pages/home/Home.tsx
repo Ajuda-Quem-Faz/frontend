b6837195a049be7caf7bd85d139e75203b9b3267
Vitor Ferreira <vitornasc673@gmail.com>
2024-02-06 16:04:13 -0300

    Alteração no banner para que se torne clicável

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import Ideia from '../../components/home/info/Info';
 import Produtos from '../../components/home/produtos-carousel/Carousel-container';
 import Presentes from '../../components/home/presentes/Presentes';
+import { Link } from 'react-router-dom';
 
 function Home() {
   useEffect(() => {
@@ -10,15 +11,17 @@ function Home() {
   return (
     <>
       <div className="lg:flex justify-between bg-light-gray">
-       
-        </div>
-        <div id="imagem" className="grid object-cover place-content-center">
+
+      </div>
+      <div id="imagem" className="grid object-cover place-content-center">
+        <Link to={`/about`}>
           <img
             src="https://ik.imagekit.io/gwq8l9cqx/Banner%20de%20site%20promo%C3%A7%C3%A3o%20de%20ver%C3%A3o%20moderno%20azul%20e%20amarelo%20(1472%20x%20431%20px)%20(2).gif?updatedAt=1707140463109"
-            alt="Cesto feito manualmente com mãos ao fundo"
+            alt="Banner Ajuda Quem faz"
           />
-        </div>
-      
+        </Link>
+      </div>
+
       <div className="container mx-auto grid grid-cols-1 gap-8 py-16 px-1">
         <Produtos />
         <Presentes />
